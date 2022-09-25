a47f8fb3efef2c27a1f256a6075a202a9acaded0
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-09-25 02:18:43 -0700

    🚀 UPDATE: updated config

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,6 +1,22 @@
 const production = {
     clientUrl: 'https://www.nfthost.app',
-	serverUrl: 'http://localhost:8080'
+	serverUrl: 'http://localhost:8080',
+    stripe: {
+        products: {
+            generator: {
+                productId: 'prod_MUTRTKzbIDNx90',
+                priceId: 'price_1LlUUMHjrZpuqKHtI8T2eCPj'
+            },
+            website: {
+                productId: 'prod_MUTS2m2Jw4BUJe',
+                priceId: 'price_1LlUVaHjrZpuqKHtyWSqAG9Z'
+            },
+            utils: {
+                productId: 'prod_MUTSj2PA5iVWHP',
+                priceId: 'price_1LlUW1HjrZpuqKHtbRc8RIyE'
+            }
+        }
+    }
 }
 
 module.exports = production
--- src/config/test.js
+++ src/config/test.js
@@ -1,6 +1,22 @@
 const test = {
     clientUrl: 'http://localhost:3000',
-	serverUrl: 'http://localhost:8080'
+	serverUrl: 'http://localhost:8080',
+    stripe: {
+        products: {
+            generator: {
+                productId: 'prod_MUjIgHsydbMGLu',
+                priceId: 'price_1LljpQHjrZpuqKHtmIxLJf2C'
+            },
+            website: {
+                productId: 'prod_MUjJIzvXHI56gw',
+                priceId: 'price_1LljrGHjrZpuqKHtqH9yBZCP'
+            },
+            utils: {
+                productId: 'prod_MUjKZNifbdGKgI',
+                priceId: 'price_1LljrcHjrZpuqKHt1x9LRWiT'
+            }
+        }
+    }
 }
 
 module.exports = test
