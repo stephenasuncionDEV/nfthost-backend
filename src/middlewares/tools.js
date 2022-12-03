13cf97f7d90ace30d2a739c0065c0ec305087fd5
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-12-02 17:14:40 -0800

    🚀 UPDATE: updated domain verification

--- src/middlewares/tools.js
+++ src/middlewares/tools.js
@@ -9,40 +9,21 @@ module.exports.ParseWebsiteData = (data) => {
     return JSON.parse(lz.decompress(lz.decodeBase64(data)));
 }
 
-module.exports.VerifyDns = async (domain) => {
+module.exports.VerifyDns = (domain) => {
     return new Promise((resolve) => {
         try {
             dns.resolveAny(domain, (err, records) => {
-                if (err) resolve({ type: 'failed', status: false });
+                if (err || !records) return resolve({ message: "Cannot find domain's DNS records.", status: false });
 
-                console.log(records)
+                const aliases = records.filter((record) => record.type === 'A');
+                if (!aliases) return resolve({ message: 'Please ask staff to verify your custom domain on Discord.', status: false });
 
-                const types = records.map((record) => record.type);
-                const aliasIdx = types.indexOf('A');
-                const cnameIdx = types.indexOf('CNAME');
+                if (!aliases[0].address.startsWith('76.76.21.')) return resolve({ message: 'Please ask staff to verify your custom domain on Discord.', status: false });
 
-                let verified = false;
-
-                if (aliasIdx !== -1) {
-                    verified = (records[aliasIdx].address === '76.76.21.241' || records[aliasIdx].address === '76.76.21.9');
-                    if (verified) resolve({
-                        type: 'alias',
-                        status: true
-                    })
-                }
-
-                if (cnameIdx !== -1) {
-                    verified = (records[cnameIdx].address.indexOf('nfthost.app') !== -1);
-                    if (verified) resolve({
-                        type: 'cname',
-                        status: true
-                    })
-                }
-
-                if (aliasIdx === -1 && cnameIdx === -1) resolve({ type: 'failed', status: false });
+                return resolve({ message: "Successfuly verified.", status: true });
             })
         } catch (err) {
-            resolve({ type: 'failed', status: false });
+            return resolve({ message: err.message, status: false });
         }
     });
 };
